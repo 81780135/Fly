e22946ecb152cf1c30069784310907750e587945
fuhao996 <17666663940@163.com>
2025-04-23 02:06:08 +0800

    付豪上传打安卓包文件和新增个人信息功能面板

--- Assets/Scripts/Core/UI/UIManager.cs
+++ Assets/Scripts/Core/UI/UIManager.cs
@@ -10,6 +10,7 @@ public class UIManager : MonoBehaviour
     public GameObject mainMenu;   // 主界面
     public GameObject planeMenu; // 战机界面
     public GameObject pveMenu; // 探索关卡界面
+    public GameObject My_Data; // 个人信息界面
 
     // 当点击"飞机养成"按钮时调用
     public void OpenPlaneMenu()
@@ -27,11 +28,26 @@ public class UIManager : MonoBehaviour
         planeMenu.SetActive(false);  // 关闭战机界面
         pveMenu.SetActive(true);  // 打开探索关卡界面
     }
-    
+
+    // 当点击"个人信息"按钮时调用
+    public void OpenMy_Data()
+    {
+        Debug.Log("按钮被点击了！"); // 添加这行
+        My_Data.SetActive(true);  // 打开个人信息界面
+    }
+
+    // 当点击"个人信息背景"按钮时调用
+    public void CloseMy_Data()
+    {
+        Debug.Log("按钮被点击了！"); // 添加这行
+        My_Data.SetActive(false);  // 关闭个人信息界面
+    }
+
     public void ReturnToMainMenu()
     {
         pveMenu.SetActive(false);   
         planeMenu.SetActive(false);
+        My_Data.SetActive(false);
         mainMenu.SetActive(true);
     }
 
