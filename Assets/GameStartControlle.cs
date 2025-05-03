5ecae46cb4d437c10c2bd9a37bca660c74b378ff
EthanNg <81780135@qq.com>
2025-05-03 21:41:57 +0800

    测试提交

--- Assets/GameStartControlle.cs
+++ Assets/GameStartControlle.cs
@@ -7,7 +7,7 @@ public class GameStartControlle : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        int a = 1;
     }
 
     // Update is called once per frame
